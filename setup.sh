9e53892097778c7a8610b14fb585adb727b3f6f3
Josh Matthews <josh@redriverit.net>
2009-07-12 20:05:24 -0500

    Fixed apt-get install & rubygems install

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ packageList="ruby ruby-dev libruby libopenssl-ruby irb rdoc ri"
 gemList="puppet facter"
 
 echo "Installing ruby packages using apt-get... "
-apt-get update &> /dev/null
-apt-get install $packageList &> /dev/null
+apt-get -qq update
+apt-get -qqq install $packageList
 if [ $? -eq 1 ] ; then
 	echo "Something broke. I quit."
 	exit 1
@@ -18,7 +18,7 @@ mkdir -p $HOME/tmp/puppet
 cd $HOME/tmp/puppet
 
 echo "Downloading current RubyGems... "
-curl -O http://rubyforge.org/frs/download.php/57643/rubygems-1.3.4.tgz &> /dev/null
+wget http://rubyforge.org/frs/download.php/57643/rubygems-1.3.4.tgz &> /dev/null
 if [ $? -eq 1 ] ; then
 	echo "Something broke. I quit."
 	exit 1
@@ -26,7 +26,7 @@ else
 	echo "done"
 fi
 
-echo "Installing RubyGems... "
+# Installing RubyGems...
 tar zxf rubygems-1.3.4.tgz &> /dev/null
 cd rubygems-1.3.4
 ruby setup.rb
@@ -36,6 +36,8 @@ if [ $? -eq 1 ] ; then
 else
 	echo "done"
 fi
+
+ln -s /usr/bin/gem1.8 /usr/bin/gem
 
 echo "Installing Puppet... "
 gem install $gemList
