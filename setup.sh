710d1e1ad13619c04cab3eb26a04b7a767efd971
Josh Matthews <josh@redriverit.net>
2009-07-12 21:21:09 -0500

    Create manifests directory and site.pp

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 packageList="ruby ruby-dev libruby libopenssl-ruby irb rdoc ri"
 gemList="puppet facter"
 
+
 echo "Installing ruby packages using apt-get... "
 apt-get -qq update
 apt-get -qqq install $packageList
@@ -11,11 +12,11 @@ if [ $? -ne 0 ] ; then
 	echo "Something broke. I quit."
 	exit 1
 else
+	mkdir -p $HOME/tmp/puppet
+	cd $HOME/tmp/puppet
 	echo "done"
 fi
 
-mkdir -p $HOME/tmp/puppet
-cd $HOME/tmp/puppet
 
 echo "Downloading current RubyGems... "
 wget http://rubyforge.org/frs/download.php/57643/rubygems-1.3.4.tgz &> /dev/null
@@ -26,6 +27,7 @@ else
 	echo "done"
 fi
 
+
 # Installing RubyGems...
 tar zxf rubygems-1.3.4.tgz
 cd rubygems-1.3.4
@@ -34,19 +36,31 @@ if [ $? -ne 0 ] ; then
 	echo "Something broke. I quit."
 	exit 1
 else
+	ln -s /usr/bin/gem1.8 /usr/bin/gem
 	echo "done"
 fi
 
-ln -s /usr/bin/gem1.8 /usr/bin/gem
 
 echo "Installing Puppet... "
-gem install $gemList
+gem install $gemList --no-rdoc --no-ri
 if [ $? -ne 0 ] ; then
 	echo "Something broke. I quit."
 	exit 1
 else
+	puppetmasterd --mkusers
+	pkill puppetmasterd
+
+	(
+	cat <<EOF
+# site.pp
+file { "/etc/sudoers":
+	owner => root, group => root, mode => 440
+}
+EOF
+	) > /etc/puppet/manifests/site.pp
 	echo "done"
 fi
+
 
 echo "Puppet successfully installed"
 
